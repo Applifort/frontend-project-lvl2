f0902757ca51e701de55fa63a8dae73ac6f86cde
GabitovArtur <coolermast@gmail.com>
2020-01-15 19:14:08 +0500

    fix default render

--- src/render/default.js
+++ src/render/default.js
@@ -5,30 +5,32 @@ const plus = '  + ';
 const minus = '  - ';
 const empty = '    ';
 
+const getPrefix = (depth) => seporator.repeat(depth);
+
 const stringify = (lastElement, data, depth) => {
   if (typeof data !== 'object') return [`${lastElement}${data}`];
-  const prefix = seporator.repeat(depth + 1);
+  const prefix = getPrefix(depth + 1);
   const strings = Object.entries(data).reduce((acc, [key, value]) => [...acc, `${prefix}${empty}${key}: ${value}`], []);
   const changedLastElement = `${lastElement}{`;
   return [changedLastElement, ...strings, `${prefix}}`];
 };
 
 const mapper = {
-  group: ({ name, value }, prefix, depth, fn) => [`${prefix}${empty}${name}: {`, ...fn(value, [], depth + 1), `${prefix}${empty}}`],
-  added: ({ name, value }, prefix, depth) => stringify(`${prefix}${plus}${name}: `, value, depth),
-  removed: ({ name, value }, prefix, depth) => stringify(`${prefix}${minus}${name}: `, value, depth),
-  changed: ({ name, value }, prefix, depth) => [
-    ...stringify(`${prefix}${minus}${name}: `, value.oldValue, depth),
-    ...stringify(`${prefix}${plus}${name}: `, value.newValue, depth),
+  group: ({ name, value }, depth, fn) => [`${getPrefix(depth)}${empty}${name}: {`, ...fn(value, [], depth + 1), `${getPrefix(depth)}${empty}}`],
+  added: ({ name, value }, depth) => stringify(`${getPrefix(depth)}${plus}${name}: `, value, depth),
+  removed: ({ name, value }, depth) => stringify(`${getPrefix(depth)}${minus}${name}: `, value, depth),
+  changed: ({ name, value }, depth) => [
+    ...stringify(`${getPrefix(depth)}${minus}${name}: `, value.oldValue, depth),
+    ...stringify(`${getPrefix(depth)}${plus}${name}: `, value.newValue, depth),
   ],
-  unchanged: ({ name, value }, prefix) => [`${prefix}${empty}${name}: ${value}`],
+  unchanged: ({ name, value }, depth) => [`${getPrefix(depth)}${empty}${name}: ${value}`],
 };
 
 export default (ast) => {
   const iter = (tree, iAcc, depth) => {
-    const prefix = seporator.repeat(depth);
+    const prefix = getPrefix(depth);
     return tree
-      .reduce((acc, node) => [...acc, ...mapper[node.type](node, prefix, depth, iter)], iAcc);
+      .reduce((acc, node) => [...acc, ...mapper[node.type](node, depth, iter)], iAcc);
   };
   return ['{', ...iter(ast, [], 0), '}'].join('\n');
 };
